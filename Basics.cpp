1560a59225a69ae77e88778fb636f9009888cf29
Gabaydulin Denis <denis.gabaydulin@corp.mail.ru>
2019-08-04 21:15:55 +0300

    + basic test infrastructure

--- Basics.cpp
+++ Basics.cpp
@@ -3,13 +3,27 @@
 //
 #include <string>
 #include <iostream>
+#include <gtest/gtest.h>
 
 using namespace std;
 
 int main() {
-    // output string
-    string str = "test";
-    cout << str << endl;
+    testing::InitGoogleTest();
+
+    RUN_ALL_TESTS();
 
     return 0;
+}
+
+TEST(BasicsTestSuite, Basics) {
+    int p = 1;
+    int q = 2;
+
+    int *x = &p;
+    int *y = &q;
+
+    cout << *x << endl; // 1
+    cout << *y << endl; // 2
+
+    ASSERT_FALSE(p == q);
 }
