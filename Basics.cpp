53792aa703d53a69e1d240e370e69a71ebe2a560
Gabaydulin Denis <denis.gabaydulin@corp.mail.ru>
2019-08-04 20:41:55 +0300

    + Basics starts here

---
+++ Basics.cpp
@@ -0,0 +1,15 @@
+//
+// Created by sherman on 04.08.2019.
+//
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // output string
+    string str = "test";
+    cout << str << endl;
+
+    return 0;
+}
