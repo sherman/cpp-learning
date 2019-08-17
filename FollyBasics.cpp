872dc3b6a0b79c4b84da0d5ca57594c3fc684013
Denis M. Gabaidulin <gabaden@gmail.com>
2019-08-17 20:15:24 +0300

    + bits manipulation

--- FollyBasics.cpp
+++ FollyBasics.cpp
@@ -6,14 +6,14 @@
 #include <folly/container/Foreach.h>
 #include <vector>
 #include "Arguments.h"
+#include <folly/Bits.h>
 
 using namespace std;
 using namespace folly;
 
 static int MAX_ELTS = 1000000;
 
-
-int main() {
+static void iterate() {
     std::string text = "text";
     std::vector<int> data(MAX_ELTS);
     for (int i = 0; i < MAX_ELTS; i++) {
@@ -24,6 +24,23 @@ int main() {
     FOR_EACH_RANGE(i, 1, 100) {
         vector<int> res = args.modifyCopy(data);
     }
+}
+
+static void bits() {
+    const int value1 = 0x11100;
+    assert(popcount(value1) == 3);
+
+    const int value2 = 0x00111001;
+    assert(popcount(value2) == 4);
+
+    const int value3 = 10; // 1010
+    const int expected = 1342177280; // 1010000000000000000000000000000
+    assert(bitReverse(value3) == expected);
+}
+
+int main() {
+    iterate();
+    bits();
 
     return 0;
 }
